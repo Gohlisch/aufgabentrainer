0658fa0b9a7ca00f861bb2f3268a457cf8945176
timgohlisch <tim.gohlisch001@stud.fh-dortmund.de>
2022-11-06 21:23:11 +0100

    feat(lib): add footnote

---
+++ src/lib/shared_components/Footnote.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export let link;
+	export let number;
+</script>
+
+<script context="module">
+    export let ascendingNumber = 0;
+</script>
+
+<style>
+    a {
+        font-size: 0.5em;
+    }
+</style>
+
+<a href={link}>{number || ++ascendingNumber}</a>
